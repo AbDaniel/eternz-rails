d14b6c0c09ef02166b6756f265897362cb13beb2
daniel <abrahamdaniel92@gmail.com>
2015-08-06 22:33:21 +0530

    Danny -[Bad Commit message] Lot of changes to get an angular page working

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,7 @@ module SpreeStore
 
     config.assets.paths << Rails.root.join("vendor", "assets", "bower_components")
     config.assets.paths << Rails.root.join("vendor", "assets", "bower_components", "bootstrap-sass-official", "assets", "fonts")
+
     config.assets.precompile << %r(.*.(?:eot|svg|ttf|woff|woff2)$)
 
   end
