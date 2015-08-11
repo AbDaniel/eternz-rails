e1dbf5a9beba16e45b3b94289db87e99af40c615
senthilkumar04 <senthilkumar.cse04@gmail.com>
2015-08-12 00:17:56 +0530

    spree strorefront is routed to root

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   # If you would like to change where this engine is mounted, simply change the :at option to something different.
   #
   # We ask that you don't use the :as option here, as Spree relies on it being the default of "spree"
-  mount Spree::Core::Engine, :at => '/backend'
+  mount Spree::Core::Engine, :at => '/'
 
-  get '/' => 'home#index'
+  #get '/' => 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
